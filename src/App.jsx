8c46f4e99b2071c95f27a1d24020bfc31a6326a7
Nitin Kumar Vasdhani <nitinkumarvasdhani786@gmail.com>
2024-01-05 13:25:57 +0530

    Some changes is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import React from 'react'
 import './App.css'
 import Loader from './Components/Loader/Loader'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <Loader/>
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../../App.css";
+const Home = () => {
+  return (
+    <>
+    <div><h1>Welcome to the Home Page!</h1></div>
+    </>
+  );
+};
+export default Home;
