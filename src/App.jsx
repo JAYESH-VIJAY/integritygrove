82183dd6dc6221971b76d2f091d2ad54ceb04d52
Nitin Kumar Vasdhani <nitinkumarvasdhani786@gmail.com>
2024-01-05 12:22:49 +0530

    Loader spelling in change only

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import Loder from './Components/Loder/Loder'
+import Loader from './Components/Loader/Loader'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-       <Loder/>
+      <Loader/>
     </div>                                                                                                                                                                    
   )
 }
