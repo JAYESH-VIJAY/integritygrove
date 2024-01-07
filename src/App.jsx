d6a41ed7bdd3bcd43989a747dc7f4e3095f6976f
jayesh-vijay <jayeshvijay649@gmail.com>
2024-01-07 15:44:37 +0530

    last

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Loader from "./components/Loader";
 import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import HomeLayout from "./layout/HomeLayout";
+
 function App() {
   const [showLoader, setShowLoader] = useState(true);
   useEffect(() => {
@@ -19,8 +20,8 @@ function App() {
   return (
     <Routes>
       <Route element={<HomeLayout />}>
-
         <Route index element={<HomePage />} />
+        <Route path="/contact" element={<HomePage />} />
       </Route>
     </Routes>
   );
