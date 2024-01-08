ce1cdaf3c1d777690429ea7d0f4a5e8e81d7d809
Ankit chopra <chopraankit1608@gmail.com>
2024-01-08 14:28:29 +0530

    interoslider image update andContact section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,27 +1,30 @@
-import Blog from "../Components/HomePage/Blog/Blog";
-import OurClient from "../Components/HomePage/Brands/OurClient";
-import Header from "../Components/HomePage/Header/Header";
-import Marquee from "../Components/HomePage/Marquee slider/Marquee";
-import Section from "../Components/HomePage/Section/Section";
-import TabService from "../Components/HomePage/Service-tab/tabService";
-import Service from "../Components/HomePage/Services/Service";
-import Testimo from "../Components/HomePage/Testimonails/Testimo";
-import ScrollX from "../Components/HomePage/VerticalScroll/ScrollX";
+// import Blog from "../Components/HomePage/Blog/Blog";
+// import OurClient from "../Components/HomePage/Brands/OurClient";
+// import Header from "../Components/HomePage/Header/Header";
+// import Marquee from "../Components/HomePage/Marquee slider/Marquee";
+// import Section from "../Components/HomePage/Section/Section";
+// import TabService from "../Components/HomePage/Service-tab/tabService";
+// import Service from "../Components/HomePage/Services/Service";
+// import Testimo from "../Components/HomePage/Testimonails/Testimo";
+// import ScrollX from "../Components/HomePage/VerticalScroll/ScrollX";
 import IntroSlider from "../components/HomePage/IntroSlider";
+import ContactSection from "../components/landingPage/ContactSection";
 
 export default function HomePage() {
   return (
     <main className="main-bg">
-      {/* <IntroSlider /> */}
-      <Header/>
-      <Section />
-      <Marquee />
+
+      <IntroSlider />
+      <ContactSection/>
+      {/* <Header/> */}
+      {/* <Section /> */}
+      {/* <Marquee /> */}
       {/* <OurClient /> */}
-      <Service />
-      <ScrollX />
-      <TabService/>
-      <Testimo/>
-      <Blog/>
+      {/* <Service /> */}
+      {/* <ScrollX /> */}
+      {/* <TabService/> */}
+      {/* <Testimo/> */}
+      {/* <Blog/> */}
     
 
     </main>
