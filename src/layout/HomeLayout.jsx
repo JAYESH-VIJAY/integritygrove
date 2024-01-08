3fe4b15f91e30b1d031bf21a7a81aa00f7954696
JAYESH-VIJAY <jayeshvijay649@gmail.com>
2024-01-08 20:52:40 +0530

    new

--- src/layout/HomeLayout.jsx
+++ src/layout/HomeLayout.jsx
@@ -1,5 +1,5 @@
 import Navbar from "../components/Navbar";
-import Footer from "../Components/Footer/Footer";
+import Footer from "./../Components/Footer/Footer";
 import { Outlet } from "react-router-dom";
 export default function HomeLayout() {
   return (
