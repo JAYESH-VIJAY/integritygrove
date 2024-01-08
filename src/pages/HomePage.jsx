bb5a00d1db4b690822a10ae19ae7578b8498f533
JAYESH-VIJAY <jayeshvijay649@gmail.com>
2024-01-08 20:41:48 +0530

    new

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,12 +2,9 @@ import IntroSlider from "../components/HomePage/IntroSlider";
 import Pricing from "../components/landingPage/Pricing";
 import Testomonials from "../components/landingPage/Testomonials";
 import OurTeam from "../components/landingPage/OurTeam";
-import Services from "../components/landingPage/services";
+import Services from "../components/landingPage/Services";
 import ContactSection from "../components/landingPage/ContactSection";
 import Projects from "../components/landingPage/Projects";
-
-
-
 export default function HomePage() {
   return (
     <main className="main-bg">
